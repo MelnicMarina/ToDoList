078e78c8ac22587be1a5b901eebd6774e0a9e4c9
Eu gen <eugen.petcoglo@gmail.com>
2024-07-23 13:49:15 +0300

    refactor(js) js implement

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 const label = document.querySelector('.task /*.box .checkmark img*/ ')
 const add = document.querySelector('button')
-const dell = document.querySelector('img')
+const dell1 = document.querySelector('img')
 const container = document.querySelector('.container')
 const input = document.querySelector('input')
-const text = document.querySelector('.checkmark')
 
-/*dell.addEventListener('click', () => {
-    label.classList.remove('task')
-})*/
+
 
 add.addEventListener('click', () => {
-    const contTask = document.createElement('div')
-    contTask.classList.add('boxTask')
-    container.appendChild(contTask)
-    contTask.appendChild(label)
-    const cloneLabel = contTask.cloneNode(true)
-    contTask.appendChild(cloneLabel)
-    text.textContent = input.value
+    const boxTask = label.cloneNode(true)
+    boxTask.querySelector('.checkmark').textContent = input.value
+    container.appendChild(boxTask)
+    input.value = ''
 
-
+    const dell = boxTask.querySelector('img')
+    dell.addEventListener('click', () =>{
+        boxTask.remove()
+    })
 })
 
-const contTask = document.createElement('div')
-contTask.classList.add('boxTask')
-container.appendChild(contTask)
-dell.addEventListener('click', () => {
-
-    contTask.querySelectorAll('.boxTask')
-    contTask.appendChild(label)
-    contTask.removeChild(label)
+dell1.addEventListener('click', () =>{
+    label.remove()
 })
+
+
+

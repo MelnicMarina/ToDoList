503aa63a74f2179c535546bf42cc5236415516da
Eu gen <eugen.petcoglo@gmail.com>
2024-07-23 19:29:52 +0300

    refactor(js,css)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const add = document.querySelector('button')
 const dell1 = document.querySelector('img')
 const container = document.querySelector('.container')
 const input = document.querySelector('input')
-
+const text1 = document.querySelector('.checkmark')
 
 
 add.addEventListener('click', () => {
@@ -16,11 +16,22 @@ add.addEventListener('click', () => {
     dell.addEventListener('click', () =>{
         boxTask.remove()
     })
+
+    const text = boxTask.querySelector(".checkmark")
+    text.addEventListener('click', () => {
+        text.classList.toggle('checkmark1')
+    })
 })
 
 dell1.addEventListener('click', () =>{
     label.remove()
 })
+
+text1.addEventListener('click', () => {
+    text1.classList.toggle('checkmark1')
+})
+
+
 
 
 
